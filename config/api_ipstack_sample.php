1702d2ddf714be41fe237f58dc407c0fdb4abaae
Björn <bjorn.olsson87@gmail.com>
2020-12-02 13:56:09 +0000

    Removed everything that weather class uses

--- config/api_ipstack_sample.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Rename this file to api_ipstack.php (remove the _sample at the end)
-// Add a valid APIkey from ipstack (https://ipstack.com/).
-return [
-    "apiKey" => "Replace this with a valid API key from ipstack"
-];
--- config/api_owm_sample.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Rename this file to api_owm.php (remove the _sample at the end)
-// Add a valid APIkey from openweathermap (https://openweathermap.org/api/one-call-api).
-return [
-    "apiKey" => "Replace this with a valid API key from openweathermap"
-];
